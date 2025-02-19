a74b765a69e8c27fe7fc05b6316e2ac203a25664
M. Özgür Karahan <m.ozgur.karahan@gmail.com>
2025-02-19 22:59:57 +0300

    added

---
+++ WhatsappDemo/WhatsappDemo/WhatsappDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappDemoApp.swift
+//  WhatsappDemo
+//
+//  Created by Mehmet Özgür Karahan on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
